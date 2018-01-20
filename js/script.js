2b1b3d0e7a74c70274b3c7723111e3548d4d49dd
Mohit Sinha <msinha132@gmail.com>
2018-01-20 14:19:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ $('a[href*=\\#]').on('click', function(event) {
 
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 900);
 });
+var nameValue = document.getElementById("fback").value;
+function WriteToFile(passForm) {
+    set fso = CreateObject("Scripting.FileSystemObject");  
+    set s = fso.CreateTextFile("C:\test.txt", True);
+    s.writeline("HI");
+    s.writeline("Bye");
+    s.writeline("-----------------------------");
+    s.Close();
+ }
