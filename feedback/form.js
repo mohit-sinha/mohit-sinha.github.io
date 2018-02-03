640b360f66b46922ce68aa9d00cc2ef1abadeb66
Mohit Sinha <msinha132@gmail.com>
2018-02-03 15:31:14 +0530

    Update form.js

--- feedback/form.js
+++ feedback/form.js
@@ -2,7 +2,7 @@
 var x = document.getElementById("form_sample");
 var createform = document.createElement('form'); // Create New Element Form
 createform.setAttribute("action", ""); // Setting Action Attribute on Form
-createform.setAttribute("method", "post"); // Setting Method Attribute on Form
+createform.setAttribute("method", "get"); // Setting Method Attribute on Form
 x.appendChild(createform);
 
 var heading = document.createElement('h2'); // Heading of Form
